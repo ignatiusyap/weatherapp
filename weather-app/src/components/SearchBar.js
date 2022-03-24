1e9b2563d462bfb0708aee82856dd4bfb6a8b448
ignatiusyap <31462590+ignatiusyap@users.noreply.github.com>
2022-03-25 04:54:54 +0800

    Implemented api call successful
    Next is to display data

--- weather-app/src/components/SearchBar.js
+++ weather-app/src/components/SearchBar.js
@@ -1,30 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [apiKey, setApiKey] = useState("");
+
+  const apiCallCredentials = { city: city, country: country, apiKey: apiKey };
   return (
-    <div className="search-bar">
-      <form>
-        <label for="city-search-box">City: </label>
-        <input
-          type="text"
-          value={city}
-          onChange={(event) => {
-            event.target.value;
+    <div>
+      <div className="api-key">
+        <form>
+          <label htmlFor="api-key-box">Api Key: </label>
+          <input
+            type="text"
+            value={apiKey}
+            onChange={(event) => {
+              setApiKey(event.target.value);
+            }}
+            id="api-key-box"
+          />
+        </form>
+        {props.apiKeyValidation === false && <p>Api key error</p>}
+      </div>
+      <div className="search-bar">
+        <form>
+          <label htmlFor="city-search-box">City: </label>
+          <input
+            type="text"
+            value={city}
+            onChange={(event) => {
+              setCity(event.target.value);
+            }}
+            id="city-search-box"
+          />
+          <label htmlFor="country-search-box">Country: </label>
+          <input
+            type="text"
+            value={country}
+            onChange={(event) => {
+              setCountry(event.target.value);
+            }}
+            id="country-search-box"
+          />
+        </form>
+        <button
+          onClick={() => {
+            props.apiCallSearchButton(apiCallCredentials);
           }}
-          id="city-search-box"
-        />
-        <label for="country-search-box">Country: </label>
-        <input
-          type="text"
-          value={country}
-          onChange={(event) => {
-            event.target.value;
+        >
+          Search
+        </button>
+        <button
+          onClick={() => {
+            setCity("");
+            setCountry("");
           }}
-          id="country-search-box"
-        />
-      </form>
-      <button onClick={}>Search</button>
-      <button onClick={}>Clear</button>
+        >
+          Clear
+        </button>
+      </div>
+      {console.log(apiCallCredentials)}
     </div>
   );
 };
--- weather-app/src/pages/DisplayWeatherAndHistory.js
+++ weather-app/src/pages/DisplayWeatherAndHistory.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "../components/SearchBar";
+import TodayWeather from "../components/TodayWeather";
+import SearchResults from "../components/SearchResults";
 
 const DisplayWeatherAndHistory = () => {
+  const [apiKeyValidation, setApiKeyValidation] = useState(true);
+  const [searchFailedSwitch, setSearchFailedSwtich] = useState(false);
+  //Fetch the results from API from input fields upon button click
+  const apiCallSearchButton = (apiCallCredentials) => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${apiCallCredentials.city},${apiCallCredentials.country}&appid=${apiCallCredentials.apiKey}`
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error === 401) {
+          setApiKeyValidation(false);
+        }
+        if (error === 404) {
+          setSearchFailedSwtich(true);
+        }
+      });
+  };
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        apiCallSearchButton={apiCallSearchButton}
+        apiKeyValidation={apiKeyValidation}
+      />
+      <SearchResults />
     </div>
   );
 };
