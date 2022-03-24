3f86c36050771dfff53a9f800e67f37a2e0e6e53
ignatiusyap <31462590+ignatiusyap@users.noreply.github.com>
2022-03-25 02:51:40 +0800

    Update SearchResults.js

--- weather-app/src/components/SearchResults.js
+++ weather-app/src/components/SearchResults.js
@@ -1,8 +1,15 @@
 import React from "react";
 import axios from "axios";
+import TodayWeather from "./TodayWeather";
+import SearchHistory from "./SearchHistory";
 
 const SearchResults = () => {
-  return <></>;
+  return (
+    <>
+      <TodayWeather />
+      <SearchHistory />
+    </>
+  );
 };
 
 export default SearchResults;
